8a72d241e40431fded7712f4e7c05ec48ab5f52f
KarelWintersky <karel.wintersky@gmail.com>
2012-09-26 07:07:11 +0400

    Next minor commit

--- www/parseit.php
+++ www/parseit.php
@@ -15,13 +15,13 @@ function print_r($var,$flag=FALSE)
     else return $ret;
 }
 
-function parser($data)
+function parser_signals($data)
 {
     $map = array('id', 'scan group', 'group', 'type', 'power', 'distance');
     $lines = preg_split('/\\r\\n?|\\n/', $data);
     foreach ($lines as $n => $str)
     {
-        $sig = explode("\t",$str);
+        $sig = explode("\t",$str); // delimeter is horisontal tab!
         foreach ($sig as $idx => $v)
         {
             $signal[$map[$idx]] = $v;
@@ -30,9 +30,31 @@ function parser($data)
     }
     return print_r($lines);
 }
+function parser_roids($data)
+{
+    $lines = preg_split('/\\r\\n?|\\n/', $data); // массив строк
+    $roids = array();
+    foreach($lines as $roid_str)
+    {
+        $roid_data = explode("\t",$roid_str);
+        $key = $roid_data[0];
+        $count = str_replace(',','',$roid_data[1]); // 1 - counnt
+        $roids[$key] += (int)$count; // count
+        // remove "," from number - http://www.cyberforum.ru/php-beginners/thread435327.html
+    }
+    return print_r($roids);
+}
+
 
 $example = (isset($_POST['parsedata'])) ? $_POST['parsedata'] : '';
 $parsed = (isset($_POST['parsedata']) && isset($_POST['parseit'])) ? parser($example) : '';
+if (isset($_POST['parse_signals']))
+    $parsed = parser_signals($example);
+elseif (isset($_POST['parse_mining']))
+    $parsed = parser_roids($example);
+else $parsed = '';
+
+
 ?>
 
 <!DOCTYPE html>
@@ -44,7 +66,9 @@ $parsed = (isset($_POST['parsedata']) && isset($_POST['parseit'])) ? parser($exa
 <body>
 <form action="<? echo $SCRIPT_NAME; ?>" method="post">
 <textarea cols="80" rows="6" resizeable name="parsedata"><? echo $example; ?></textarea><br>
-<input type="submit" name="parseit" value="Parse it! >>> ">
+    <input type="submit" name="parse_signals" value="Parse signals >>> ">
+    <input type="submit" name="parse_mining" value="Parse Asteroid Data >>> ">
+    <input type="reset" value="Clear data!">
 </form>
 <hr>
 <?php echo $parsed; ?>
